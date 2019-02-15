4942af127e22768a645f6983c1c6a8a9aab75667
SurrealProducts <surrealproductsdesign@gmail.com>
2019-02-15 14:45:32 -0800

    Simple Timer that counts up 1 per Second

---
+++ SimpleScripts/SimpleTimer.cs
@@ -0,0 +1,79 @@
+/* This content is licensed under the terms of the Creative Commons Attribution 4.0 International License.
+ * When using this content, you must:
+ * �    Acknowledge that the content is from the Sansar Knowledge Base.
+ * �    Include our copyright notice: "� 2017 Linden Research, Inc."
+ * �    Indicate that the content is licensed under the Creative Commons Attribution-Share Alike 4.0 International License.
+ * �    Include the URL for, or link to, the license summary at https://creativecommons.org/licenses/by-sa/4.0/deed.hi (and, if possible, to the complete license terms at https://creativecommons.org/licenses/by-sa/4.0/legalcode.
+ * For example:
+ * "This work uses content from the Sansar Knowledge Base. � 2017 Linden Research, Inc. Licensed under the Creative Commons Attribution 4.0 International License (license summary available at https://creativecommons.org/licenses/by/4.0/ and complete license terms available at https://creativecommons.org/licenses/by/4.0/legalcode)."
+ 
+ 
+ 
+    SurrealProducts Notes:
+    Simple Timer that counts up 1 every second. Code provided by OldVamp, modified by SurrealProducts.
+
+    Drag this script into a Mesh Object in Editor. Save and Build Scene. In-world when you hover your mouse over the Object seconds counter will go up.
+ 
+ 
+ 
+ 
+ 
+ */
+
+using Sansar.Script;
+using Sansar.Simulation;
+using System;
+
+// A small starter script.
+public class DisplayText : SceneObjectScript
+{
+    #region EditorProperties
+    // This interaction will have a default prompt of "Click Me!"
+    // Public fields show in the object properties after being added to a script.
+    // An Interaction public property makes the script clickable.
+    [DefaultValue("Click Me!")]
+    public Interaction SetTextHere;
+
+
+    //float tick = 1.0f / 8.0f; //set updates per second, max 90
+    float tick = 1;
+    int Seconds = 0;
+    string SecondsText = "";
+
+
+
+    #endregion
+
+
+
+
+    // Init() is where the script is setup and is run when the script starts.
+    public override void Init()
+    {
+        // Subscribe to interaction events to do something when the object is clicked.
+        SetTextHere.Subscribe(OnClick);
+
+        Timer.Create(TimeSpan.FromSeconds(tick), TimeSpan.FromSeconds(tick), TimerTick);
+
+
+
+    }
+
+
+        public void TimerTick()
+        {
+        //ticktock
+        Seconds++;
+        SecondsText = Seconds.ToString();
+        SetTextHere.SetPrompt(SecondsText);
+        }
+
+
+
+
+    public void OnClick(InteractionData data)
+        {
+            //do nothing on click
+        }
+
+}
